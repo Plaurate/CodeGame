91eb49efa9544a915cfed2beb3337fb0306aaf02
Plaurate <152171932+Plaurate@users.noreply.github.com>
2024-03-22 13:10:05 +0200

    Add files via upload

--- CodeGame.py
+++ CodeGame.py
@@ -9,4 +9,28 @@ for i in range(n):
 
 # Pārbauda kāds kods tiek printēts
 # print(*kods)
-    
+
+minejumu_skaits = 0
+
+#Izmantojot join, apvieno sarakstu ar 4 cipariem vienā izmantojamā koda virknē.
+x = "".join(str(num) for num in kods)
+print(x)
+
+
+pareizi_cipari = 0
+
+while True:
+    minejums = input("Atmini 4 ciparu kodu!: ")
+    if minejums.lower() == "stop":
+        break
+    elif x == minejums:
+        print("Pareizi!")
+        print(f"Tu uzminēji kodu {minejumu_skaits} minējumos!")
+        for i in range(n):
+            kods.clear()
+            kods.append(randint(0,9))
+        x = "".join(str(num) for num in kods)
+    else:
+        print(f"Pareizi novietoti ir {pareizi_cipari} cipari no 4")
+        minejumu_skaits += 1
+print("Paldies par spēlēšanu!")
