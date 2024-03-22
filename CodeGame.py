23d0654a4e5ea8cdef3ed131e2068dabc078059c
Plaurate <152171932+Plaurate@users.noreply.github.com>
2024-03-22 11:56:23 +0200

    Add files via upload

---
+++ CodeGame.py
@@ -0,0 +1,12 @@
+from random import randint #Random Integer
+
+kods=[]
+n=4
+
+#For funkcija kas pievieno kodam četras nejauši izvēlētas vērtības vērtības
+for i in range(n):
+    kods.append(randint(0,9))
+
+# Pārbauda kāds kods tiek printēts
+# print(*kods)
+    
