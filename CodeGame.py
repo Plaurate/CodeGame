a9fb5bb366a04afb384b12531f08dc2e95228150
Plaurate <152171932+Plaurate@users.noreply.github.com>
2024-04-04 23:33:38 +0300

    Add files via upload

--- CodeGame.py
+++ CodeGame.py
@@ -1,4 +1,5 @@
 from random import randint #Random Integer
+from colorama import Fore
 
 kods=[]
 n=4
@@ -7,35 +8,41 @@ n=4
 for i in range(n):
     kods.append(randint(0,9))
 
+#Pārvērš vērtības sarakstā par string tālākai lietošanai
+string_kods = [str(element) for element in kods]
 # Pārbauda kāds kods tiek printēts
 # print(*kods)
 
-minejumu_skaits = 0
+minejumu_skaits = 1
 
 #Izmantojot join, apvieno sarakstu ar 4 cipariem vienā izmantojamā koda virknē.
 x = "".join(str(num) for num in kods)
-print(x)
 
-
-pareizi_cipari = 0
+#Testēšanas nolūkam, iedots pirmais kods
+# print(x)
 
 while True:
-    minejums = input(f"Atmini 4 ciparu kodu!: ")
+    minejums = input(Fore.BLUE + f"Atmini 4 ciparu kodu!: ")
     if minejums.lower() == "stop":
         break
     elif x == minejums:
-        print("Pareizi!")
-        print(f"Tu uzminēji kodu {minejumu_skaits} minējumos!")
+        print(Fore.GREEN + "Pareizi!")
+        print(f"Tu uzminēji kodu {minejumu_skaits} minējumā/os!")
         for i in range(n):
             kods.clear()
             kods.append(randint(0,9))
         x = "".join(str(num) for num in kods)
     else:
-        
         a = list(minejums)
-        if a[0] == kods[0]:
-           pareizi_cipari = pareizi_cipari + 1 
 
-        print(f"Pareizi novietoti ir {pareizi_cipari} cipari no 4")
+        def cipari(saraksts1, saraksts2):
+            pareizi = 0
+            for item1, item2 in zip(saraksts1, saraksts2):
+                if item1 == item2:
+                    pareizi += 1
+            return pareizi
+        pareizi_cipari = cipari(string_kods, a)
+
+        print(Fore.MAGENTA + f"Pareizi novietoti ir {pareizi_cipari} cipari no 4")
         minejumu_skaits += 1
-print("Paldies par spēlēšanu!")
+print(Fore.YELLOW + "Paldies par spēlēšanu!")
